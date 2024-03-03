4e2099ab20a4b55ebe9200c3f704b9acf6b874b1
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-03-03 00:40:40 -0300

    carga masiva del proyecto

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+//declarar variables
+
+let precioInicial = 400000;
+let cantidadInicial = 0;
+
+let precio = document.querySelector(".precio");
+let cantidad = document.querySelector(".cantidad");
+let total = document.querySelector(".total");
+
+precio.innerHTML = precioInicial;
+cantidad.innerHTML = cantidadInicial;
+total.innerHTML = precioInicial;
+
+
+
+// Función para incrementar la cantidad
+// function incrementarCantidad() {
+//     cantidad++;
+//     cantidadSpan.innerHTML = cantidad;
+//     calcularTotal();
+// }
+
+// Función para decrementar la cantidad
+// function decrementarCantidad() {
+//     if (cantidad > 0) {
+//         cantidad--;
+//         cantidadSpan.innerHTML = cantidad;
+//         calcularTotal();
+//     }
+// }
+
+// Función para calcular el total a pagar
+// function calcularTotal() {
+//     const total = precio * cantidad;
+//     totalSpan.innerHTML = total;
+// }
+
+// Inicializar el precio inicial
+// precioSpan.innerHTML = precio;
